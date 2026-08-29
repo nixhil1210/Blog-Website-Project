2506b80eeb35c24c2fa947ac4db430de4441bdad
NIkhil Raj <nikhilraj2000q@gmail.com>
2026-08-29 21:07:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById("menuBtn");
+
+const navigation = document.getElementById("navLinks");
+
+menuButton.addEventListener("click", function () {
+
+    navigation.classList.toggle("show");
+
+});
